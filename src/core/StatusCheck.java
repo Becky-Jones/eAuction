8c30716fec3986ca38af233eb5c22f2164838da2
Becky-Jones <becky4_1@btinternet.com>
2019-04-01 11:01:45 +0100

    Implement status check

--- src/core/StatusCheck.java
+++ src/core/StatusCheck.java
@@ -1,4 +1,23 @@
 package core;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class StatusCheck {
+
+    public List<Auction> auctions = new LinkedList<>();
+    Integer delay;
+
+    public StatusCheck(List<Auction> auctions, int seconds) {
+        this.auctions = auctions;
+        setSeconds(seconds);
+    }
+
+    private synchronized void setSeconds(int seconds) {
+        this.delay = seconds * 1000;
+    }
+
+    public int getSeconds() {
+        return delay;
+    }
 }
