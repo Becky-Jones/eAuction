90d7b5c678a8604d58075a663f8591746d0c777b
Becky Jones <becky4_1@btinternet.com>
2019-04-03 18:04:37 +0100

    If reserve price is met close auction

--- src/core/Auction.java
+++ src/core/Auction.java
@@ -38,6 +38,11 @@ public class Auction {
      */
     public void placeBid(Bid userBid) {
         this.bids.add(userBid);
+
+        if (userBid.getAmount() == reservePrice) {
+            // reserve price as been met, auction must be closed
+            close();
+        }
     }
 
     /*
